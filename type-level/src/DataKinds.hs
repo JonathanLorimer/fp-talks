7881d3165d5ee9686eeb3a1c0650b58340e0ba62
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-11-14 14:59:26 -0500

    type-level: fixed small errors

--- type-level/src/DataKinds.hs
+++ type-level/src/DataKinds.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE KindSignatures #-}
+
+
+module DataKinds where
 
 data Nat = Zero | Succ Nat
 
-data Vec :: Nat -> Type -> Type where
+data Vec :: Nat -> * -> * where
   Nil :: Vec 'Zero a
   (:*) :: a -> Vec n a -> Vec ('Succ n) a
 deriving instance (Show a) => Show (Vec n a)
 
-strange :: Vec Char Int -> Vec ('Succ Char) Int
-strange = undefined
+-- strange :: Vec Char Int -> Vec ('Succ Char) Int
+-- strange = undefined
--- type-level/src/GADTs.hs
+++ type-level/src/GADTs.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE KindSignatures #-}
 
 module GADTs where
 
 data Zero
 data Succ n
 
-data Vec :: Type -> Type -> Type where
+data Vec :: * -> * -> * where
   Nil :: Vec Zero a
   (:*) :: a -> Vec n a -> Vec (Succ n) a
 deriving instance (Show a) => Show (Vec n a)
